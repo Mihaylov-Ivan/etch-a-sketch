d94a0e7f20f0137d9e71244e7cdfc54508bcae01
ivamih2k <mihaylov.ivan@siemens.com>
2023-05-22 00:08:25 +0100

    Setup the grid for coloring

--- JS/index.js
+++ JS/index.js
@@ -1,34 +1,54 @@
 const body = document.querySelector('body');
 let numberOfSquares = 0;
 let cells = [];
-
-function setBodyCSS()
-{
-    let screenWidth = window.innerWidth;
-    let screenHeight = window.innerHeight;
-    let w = screenWidth, h=screenHeight;
-
-    if(screenWidth >= screenHeight) w = h;
-    if(screenWidth <= screenHeight) h = w;
-
-    body.style.cssText = `grid: repeat(${numberOfSquares}, 1fr) / repeat(${numberOfSquares}, 1fr);
-                          justify-items: center; height: ${h}px; width: ${w}px; margin: auto;`;
-}
-
-function createCells(cell){
-    cell.style.cssText = 'width: 100%; height: 100%;';
-    body.appendChild(cell);
-}
+let mouseDown = false;
+let currentCell = "";
 
 do numberOfSquares = prompt("Please, choose a gird size between 5 and 100.");
 while (numberOfSquares < 5 || numberOfSquares > 100);
 
 setBodyCSS();
 
+window.addEventListener('resize', setBodyCSS);
+
 for(let i=0; i<numberOfSquares**2; i++){
     cells[i] = document.createElement(`cell${i+1}`);
 }
 
-cells.forEach(createCells);
+cells.forEach(addCell);
 
-window.addEventListener('resize', setBodyCSS);
+body.onmouseover = function(e) {
+    // console.log(e);
+
+    // currentCellName = e.target.localName;
+    // let cell = document.querySelector(currentCellName);
+    // cell.style.cssText = "background-color: black;";
+}
+
+body.onmousedown = function(e) {
+    console.log(e);
+}
+
+function setBodyCSS()
+{
+    let screenWidth = window.innerWidth, 
+        screenHeight = window.innerHeight;
+
+    if(screenWidth >= screenHeight) screenWidth = screenHeight;
+    if(screenWidth <= screenHeight) screenHeight = screenWidth;
+
+    body.style.cssText = `grid: repeat(${numberOfSquares}, 1fr) / repeat(${numberOfSquares}, 1fr);
+                          justify-items: center; height: ${screenHeight}px; width: ${screenWidth}px; margin: auto;`;
+}
+
+function addCell(cell){
+    cell.style.cssText = 'width: 100%; height: 100%;';
+    cell.classList.add("cell");
+    body.appendChild(cell);
+}
+
+// function addCellColor(currentCellName){
+//     let cell = document.querySelector(currentCellName);
+
+//     cell.style.cssText = "background-color: black;";
+// }
