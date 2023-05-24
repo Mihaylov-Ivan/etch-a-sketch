466aab80eb406d1c6f9634a9fad7067fc9425c9d
ivamih2k <mihaylov.ivan@siemens.com>
2023-05-24 17:11:25 +0100

    Working sketch grid

--- JS/index.js
+++ JS/index.js
@@ -1,69 +1,66 @@
+// VARIABLES
 const body = document.querySelector('body');
+const grid = document.querySelector('#grid');
+const restartBtn = document.querySelector('#restart')
 let numberOfSquares = 0;
 let cells = [];
+let makeBlack = false;
 
-do numberOfSquares = prompt("Please, choose a gird size between 5 and 100.");
-while (numberOfSquares < 5 || numberOfSquares > 100);
+// ------------------------------------------------------------
+// CODE
 
-setBodyCSS();
+// start the sketch when website loads
+start();
+// restart when button clicked
+restartBtn.onclick = () => location.reload();
 
-window.addEventListener('resize', setBodyCSS);
+// Make the grid square and responsive to window resize
+setGridCSS();
+window.addEventListener('resize', setGridCSS);
 
-for(let i=0; i<numberOfSquares**2; i++){
-    cells[i] = document.createElement(`cell${i+1}`);
-}
-
+// create the cell elements and append them to the grid div as children
+for(let i=0; i<numberOfSquares**2; i++)
+    cells[i] = document.createElement('div');
 cells.forEach(addCell);
 
+// Get all the cells
+// Add event on mouse click, to toggle changing the cell bg color to black on hover over cell
 let cellElements = Array.from(document.querySelectorAll('.cell'));
+grid.onmousedown = () => {
+    if (makeBlack) makeBlack=false;
+    else if (!makeBlack) makeBlack=true;
 
-console.log(cellElements);
+    cellElements.forEach((cellElement) => {
+        cellElement.addEventListener('mouseover',() => {
+            if (makeBlack) cellElement.classList.add('black');
+        })
+    });
+}
 
-cellElements.forEach(cellElement => function() {
-    cellElement.addEventListener('mouseover',() => {
-        cellElement.classList.add('black');
-    })
+// ------------------------------------------------------------
+// FUNCTIONS
 
-    console.log(cellElement.classList);
+// Prompt which asks for grid size, limited from 5x5 to 100x100
+function start(){
+    do numberOfSquares = prompt("Please, choose a gird size between 5 and 100.");
+    while (numberOfSquares < 5 || numberOfSquares > 100);
+}
 
-});
-
-
-// body.onmouseover = function(e) {
-
-
-
-//     // console.log(e);
-
-//     // currentCellName = e.target.localName;
-//     // let cell = document.querySelector(currentCellName);
-//     // cell.style.cssText = "background-color: black;";
-// }
-
-// body.onmousedown = function(e) {
-//     console.log(e);
-// }
-
-function setBodyCSS()
+function setGridCSS()
 {
-    let screenWidth = window.innerWidth, 
-        screenHeight = window.innerHeight;
+    let gridWidth = grid.offsetWidth, 
+        gridHeight = grid.offsetHeight;
 
-    if(screenWidth >= screenHeight) screenWidth = screenHeight;
-    if(screenWidth <= screenHeight) screenHeight = screenWidth;
+    // Make the grid a square, with a side equal to the smaller screen size length
+    if(gridWidth >= gridHeight) gridWidth = gridHeight;
+    if(gridWidth <= gridHeight) gridHeight = gridWidth;
 
-    body.style.cssText = `grid: repeat(${numberOfSquares}, 1fr) / repeat(${numberOfSquares}, 1fr);
-                          justify-items: center; height: ${screenHeight}px; width: ${screenWidth}px; margin: auto;`;
+    grid.style.cssText = `grid: repeat(${numberOfSquares}, 1fr) / repeat(${numberOfSquares}, 1fr);
+                          justify-items: center; height: ${gridHeight}px; width: ${gridWidth}px; margin: auto;`;
 }
 
 function addCell(cell){
     cell.style.cssText = 'width: 100%; height: 100%;';
     cell.classList.add("cell");
-    body.appendChild(cell);
+    grid.appendChild(cell);
 }
-
-// function addCellColor(currentCellName){
-//     let cell = document.querySelector(currentCellName);
-
-//     cell.style.cssText = "background-color: black;";
-// }
